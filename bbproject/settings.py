d9423d8a4b551177ee45ac2ca30c87ea4bb708ea
SholaAguns <106911290+SholaAguns@users.noreply.github.com>
2025-10-04 23:49:21 +0100

    use secrets

--- bbproject/settings.py
+++ bbproject/settings.py
@@ -23,12 +23,12 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mj+7uhpqevx&tkf9^8$=0y6r1=teg78c6(@+(e041f1$59)s@)'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
